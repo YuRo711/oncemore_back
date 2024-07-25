67bca89ab10b756b22f4b46238039770b23ac506
YuRo711 <rohlinay@mail.ru>
2024-07-25 14:08:02 +0300

    login

---
+++ controllers/login.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+const { JWT_SECRET } = require('../utils/config');
+const UnauthorizedError = require('../utils/errors/unauthorized-err');
+
+
+module.exports.login = (req, res, next) => {
+    const { email, password } = req.body;
+
+    User.findOne({ email }).select('+password')
+        .then((user) => {            
+            if (!user) {
+                const error = new Error();
+                error.name = 'LoginError';
+                return Promise.reject(error);
+            }
+
+            return bcrypt.compare(password, user.password)
+                .then((matched) => {
+                    if (!matched) {
+                        const error = new Error();
+                        error.name = 'LoginError';
+                        return Promise.reject(error);
+                    }
+                    const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
+                        expiresIn: "7d",
+                    });
+                    res.send({ token });
+                    return Promise.resolve();
+                });
+        })
+        .catch((err) => {
+            if (err.name === 'LoginError') {
+                next(new UnauthorizedError('Login error'));
+            } else {
+                next(err);
+            }
+        })
+}
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const userRouter = require('./users');
-// const { login } = require('../controllers/login');
+const { login } = require('../controllers/login');
 const { createUser } = require('../controllers/users');
 const NotFoundError = require('../utils/errors/not-found-err');
 const { validateUserLogin, validateUserData } = require('../middlewares/validator');
 
-// router.post('/signin', validateUserLogin, login);
+router.post('/signin', validateUserLogin, login);
 router.post('/signup', validateUserData, createUser);
 
 router.use('/users/', userRouter);
